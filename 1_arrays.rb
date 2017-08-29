5bf60ef29b713e3000b6c46c91238c78627c225a
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-08-29 19:28:29 +0100

    Exercise 1.5

--- 1_arrays.rb
+++ 1_arrays.rb
@@ -1,4 +1,5 @@
 puts [1, 2, 3, 4, 5].map {|x| x + 1 }
 puts [1,3,5,4,2].sort
 puts [1, 3, 5, 4, 2].map {|x| x + 1}.sort
-puts (1..5).to_a.inject(0){|sum,x| sum + x }
+puts (1..5).to_a.inject(0){|sum,x| sum + x}
+puts (1..5).to_a.inject(0){|sum,x| sum + x} * 2
