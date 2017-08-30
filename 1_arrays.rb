0c2c5d73a00a5cea13e3e2a48caaa6b6fd3c86f2
keith(mac) <ka33145@googlemail.com>
2017-08-30 22:55:53 +0100

    Clean up code for 1_array.rb

--- 1_arrays.rb
+++ 1_arrays.rb
@@ -1,5 +1,17 @@
-puts [1, 2, 3, 4, 5].map {|x| x + 1 }
-puts [1,3,5,4,2].sort
-puts [1, 3, 5, 4, 2].map {|x| x + 1}.sort
-puts (1..5).to_a.inject(0){|sum,x| sum + x}
-puts (1..5).to_a.inject(0){|sum,x| sum + x} * 2
+puts "Challenge 1"
+puts  "1.[1, 2, 3, 4, 5] => [2, 3, 4, 5, 6]"
+p (1..5).map{|number| number + 1}
+
+puts "","2. [1, 3, 5, 4, 2] => [1, 2, 3, 4, 5]"
+array1 = [1,3,5,4,2]
+p array1.sort
+
+puts "","3. [1, 3, 5, 4, 2] => [2, 3, 4, 5, 6] "
+p array1.sort.map{|number| number + 1}
+
+puts "","4.1, 2, 3, 4, 5] => 15 (the sum)"
+p (1..5).to_a.inject(0) {|sum ,x| sum + x}
+
+puts "","5.[1, 2, 3, 4, 5] => 30 (twice the sum)"
+p (1..5).to_a.inject(0) {|sum ,x| sum + x} * 2
+puts ""
