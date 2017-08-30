a3b41191c3a695bb707f5cca3778b28d69bd423b
keith(mac) <ka33145@googlemail.com>
2017-08-30 23:05:15 +0100

    Clean up challenge 2_hashed.rb and fix bugs with exercise 2.3 & 2.4

--- 2_hashes.rb
+++ 2_hashes.rb
@@ -1,6 +1,18 @@
-hash = { a: 1, b: 2 }
-hash.each {|k,v| hash[k] = v + 1}   # => { a: 2, b: 3 }
+puts "Challenge 2"
+puts "1.{ a: 1, b: 2 } => { a: 2, b: 3 }"
+hash1 =  {a: 1, b: 2}
+hash1.each{|key,value| hash1[key] = value + 1}
+p hash1 
 
-{ a: 2, b: 5, c: 1 }.values.sort    # => [1, 2, 5]
-
-{ a: 1, b: 2, c: 3 }.invert         # => {"1"=>"a", "2"=>"b", "3"=>"c"}
+puts
+#puts '2.{ a: 2, b: 5, c: 1 } => { a: 1, b: 2, c: 5 }'
+hash2 = { a: 2, b: 5, c: 1 }
+#hash2.each {|key,value| puts hash2[key]} #Not working
+puts
+puts "3.{ a: 2, b: 5, c: 1 } => [1, 2, 5]"
+p hash2.values.sort
+#values return only values of hash and not the key as an as an array
+puts
+puts '4.{ a: 1, b: 2, c: 3 } => { "1"=>"a", "2"=>"b", "3"=>"c" }'
+hash3 = { a: 1, b: 2, c: 3 }
+p hash3.map{ |k, v| [k.to_s, v.to_s] }.to_h.invert
